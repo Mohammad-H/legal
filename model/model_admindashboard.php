8751d6ea0d86058494c619e682ab37aae63435ed
mehrabani <moohammadh55@gmail.com>
2018-03-06 09:44:32 +0330

    listing data by ajax and filtering by coloum datatables dashboard

--- model/model_admindashboard.php
+++ model/model_admindashboard.php
@@ -5,16 +5,20 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 class model_admindashboard extends Eloquent {
 
      // 1000 row
-	 protected $table = "user_details";
+	 protected $table = "user_details1";
+	 protected $hidden=['password'];
+	 protected $guarded = [];
     // protected $hidden = ['password','status'];
-
-    public function __construct()
-    {
-//        parent::__construct();
-    }
 
     public function getAllRow()
     {
-    	return self::all();
+        return self::all()->toArray();
+    }
+
+    public function countcol()
+    {
+        $e = Model::$conn->prepare("select * from `user_details1`");
+        $e->execute();
+        return $e->columnCount();
     }
 }
--- public/admin/assets/js/pages/datatables_data_sources.js
+++ public/admin/assets/js/pages/datatables_data_sources.js
@@ -39,8 +39,7 @@ $(function() {
     });
 
 
-    // HTML sourced data
-    $('.datatable-html').dataTable();
+
 
 
     // AJAX sourced data
@@ -49,47 +48,8 @@ $(function() {
     });
 
 
-    // Javascript sourced data
-    var dataSet = [
-        ['Trident','Internet Explorer 4.0','Win 95+','4','X'],
-        ['Trident','Internet Explorer 5.0','Win 95+','5','C'],
-        ['Trident','Internet Explorer 5.5','Win 95+','5.5','A'],
-        ['Trident','Internet Explorer 6','Win 98+','6','A'],
-        ['Gecko','Firefox 1.0','Win 98+ / OSX.2+','1.7','A'],
-        ['Gecko','Firefox 1.5','Win 98+ / OSX.2+','1.8','A'],
-        ['Gecko','Firefox 2.0','Win 98+ / OSX.2+','1.8','A'],
-        ['Gecko','Firefox 3.0','Win 2k+ / OSX.3+','1.9','A'],
-        ['Gecko','Camino 1.0','OSX.2+','1.8','A'],
-        ['Gecko','Camino 1.5','OSX.3+','1.8','A'],
-        ['Webkit','Safari 1.2','OSX.3','125.5','A'],
-        ['Webkit','Safari 1.3','OSX.3','312.8','A'],
-        ['Webkit','Safari 2.0','OSX.4+','419.3','A'],
-        ['Presto','Opera 7.0','Win 95+ / OSX.1+','-','A'],
-        ['Presto','Opera 7.5','Win 95+ / OSX.2+','-','A'],
-        ['Misc','NetFront 3.1','Embedded devices','-','C'],
-        ['Misc','NetFront 3.4','Embedded devices','-','A'],
-        ['Misc','Dillo 0.8','Embedded devices','-','X'],
-        ['Misc','Links','Text only','-','X']
-    ];
-
-    $('.datatable-js').dataTable({
-        data: dataSet,
-        columnDefs: []
-    });
 
 
-    // Nested object data
-    $('.datatable-nested').dataTable({
-        ajax: 'public/admin/assets/demo_data/tables/datatable_nested.json',
-        columns: [
-            {data: "name[, ]"},
-            {data: "hr.0" },
-            {data: "office"},
-            {data: "extn"},
-            {data: "hr.2"},
-            {data: "hr.1"}
-        ]
-    });
 
 
     // Generate content for a column
@@ -100,7 +60,7 @@ $(function() {
             data: null,
             defaultContent: "<button class='label label-default'>Show</button>"
         },
-        { 
+        {
             orderable: false,
             targets: [0, 2]
         }]
